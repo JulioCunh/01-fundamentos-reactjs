0093c0be93c01ead0590d66a154c04064ea0c352
JulioCunh <fcezarf@gmail.com>
2022-08-19 18:05:32 -0300

    Propriedade

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {Post} from './Post';
+
+export function App() {
+
+  return (
+    <div>
+    <Post 
+      authot="Julio Cunha" 
+      content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo sunt veritatis, corporis quo atque distinctio adipisci quas mollitia, inventore rem quasi ipsum consectetur ut aspernatur nisi officia delectus doloribus. Eos." 
+    />
+    </div>    
+
+  )
+}
+
---
+++ src/Post.jsx
@@ -0,0 +1,8 @@
+export function Post() {
+  return (
+    <div>
+      <strong>Julio Cunha</strong>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo sunt veritatis, corporis quo atque distinctio adipisci quas mollitia, inventore rem quasi ipsum consectetur ut aspernatur nisi officia delectus doloribus. Eos.</p>
+    </div>
+  )
+}
